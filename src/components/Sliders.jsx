50a7c17bfbf5b0cce65c3fc3f310980d5be18f99
sha2311 <sha31417@gmail.com>
2024-12-03 23:19:56 +0600

    res train& others

--- src/components/Sliders.jsx
+++ src/components/Sliders.jsx
@@ -13,16 +13,16 @@ const Sliders = () => {
         slidesToScroll: 1
       };
   return (
-    <div className='h-[537px] flex items-center'>
+    <div className='md:h-[537px] h-[400px] max-w-[375px] md:max-w-[1440px] flex items-center'>
       <div className='h-[365px] mt-32  w-3/4 mx-auto '>
       <Slider {...settings}>
         {sliderData.map((value, index) => (
             <div className='' key={index}>
               <div className='flex justify-center items-center pb-5'> <img src={value.image} alt="" /></div>
                <div className='text-white flex flex-col justify-center items-center'>
-               <p className=' text-center text-[28px] px-8 font-light font-work leading-tight mb-4 '>{value.text} </p>
-               <h6 className='text-[24px] font-semibold font-work text-[#0FF1F6]'>{value.title} </h6>
-               <p className='text-xl font-work text-[#96ACAF] font-normal mb-5'>{value.des} </p>
+               <p className=' text-center md:text-[28px] px-8 font-light font-work leading-tight mb-4 '>{value.text} </p>
+               <h6 className='md:text-[24px] font-semibold font-work text-[#0FF1F6]'>{value.title} </h6>
+               <p className='md:text-xl font-work text-[#96ACAF] font-normal mb-5'>{value.des} </p>
                </div>
             </div>
         ))}
--- src/components/Train.jsx
+++ src/components/Train.jsx
@@ -3,18 +3,18 @@ import { assets, trainMap } from '../assets/asset'
 
 const Train = () => {
   return (
-    <section className='max-w-[1344px] h-[564px] bg-cover bg-no-repeat mx-10 rounded-large' style={{backgroundImage:`url(${assets.bgTrain})`}}>
+    <section className='max-w-[1344px] h-[564px] bg-cover bg-no-repeat md:mx-10 mx-5 rounded-large' style={{backgroundImage:`url(${assets.bgTrain})`}}>
       <div className='flex items-center  justify-end '>
-      <div className='my-24 text-white p-8  font-montserrat justify-end flex flex-col items-center '>
-          <h2  className="text-[40px] font-semibold ">Train your aiDR on your...</h2>
-          <p className='text-[40px] font-medium italic text-[#0FF1F6] pr-32'>prefered email st|</p>
-          <p className='w-3/5 font-light text-[24px] font-work'>You’re in control. Train your aiDR on elements of your Marketing strategy.</p>
+      <div className='md:my-24 text-white p-8  font-montserrat justify-end flex flex-col items-center '>
+          <h2  className="text-[28px] text-center md:text-left  md:text-[40px] font-semibold ">Train your aiDR on your...</h2>
+          <p className='md:text-[40px] text-[28px]  font-medium italic text-[#0FF1F6] md:pr-32'>prefered email st|</p>
+          <p className='md:w-3/5 font-light md:text-[24px] text-[18px] mt-4  mb:mt-0 font-work'>You’re in control. Train your aiDR on elements of your Marketing strategy.</p>
 
-          <div className=' w-3/5 grid grid-cols-2 gap-5 mt-8'>
+          <div className=' md:w-3/5 grid md:grid-cols-2 gap-5 mt-8'>
               {trainMap.map((value, index) => (
                 <div key={index}>
                     
-                      <p className=' flex items-center gap-2 text-[18px] font-montserrat font-medium text-nowrap'><img src={value.image}/> <span>{value.text} </span></p>
+                      <p className=' flex items-center gap-2 md:text-[18px] text-base font-montserrat font-medium text-nowrap'><img src={value.image}/> <span>{value.text} </span></p>
                     
                 </div>
               ))}
